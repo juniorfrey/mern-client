ec8a65d44c91ad56ef943da5df03bbf0394e66ac
Fredys Fernandez <juniorfrey2990gmail.com>
2021-12-23 14:18:38 -0500

    Se agrega carpeta para independizar la pagina de <mi perfil>

--- src/pages/AccountPage/AccountPage.js
+++ src/pages/AccountPage/AccountPage.js
@@ -1,7 +1,7 @@
 import { Container, Row, Col, Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import useAuth from "../Auth/useAuth";
-import routes from "../helpers/routes";
+import useAuth from "../../Auth/useAuth";
+import routes from "../../helpers/routes";
 export default function AccountPage() {
     const {user} = useAuth();
     
@@ -9,19 +9,19 @@ export default function AccountPage() {
     <Container>
       <Row className="mt-4">
         <Col xs={{ span: 12 }} className="text-center">
-          <img
-            src="/img/male_avatar.svg"
-            alt="Mi perfil"
-            style={{
-              width: "200px",
-              height: "200px",
-              borderRadius: "50%",
-              objectFit: "cover",
-            }}
-          />
+            <img
+                src="/img/male_avatar.svg"
+                alt="Mi perfil"
+                style={{
+                width: "200px",
+                height: "200px",
+                borderRadius: "50%",
+                objectFit: "cover",
+                }}
+            />
         </Col>
         <Col className="mt-4">
-          <Card className="p-2">
+          <Card className="p-4 mx-auto" style={{ maxWidth:'360px' }}>
             <p className="text-center">
               <b>Nombre: </b> {user.name}{" "}
             </p>
